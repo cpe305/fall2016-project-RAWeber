de8c1f7e14bda341dbf86584f3de8d17ec629d00
Quangder <quincyquansyquang@gmail.com>
2016-10-19 23:08:14 -0700

    Skeleton Descriptions
    Tower and TowerStore descriptions

--- core/src/main/java/com/rawprogramming/games/Towers/Tower.java
+++ core/src/main/java/com/rawprogramming/games/Towers/Tower.java
@@ -10,4 +10,36 @@ public class Tower {
 	private TowerUpgrade upgrade2;
 	
 	private TowerAttack attack;
+	
+	public Tower(String name, int cost){
+		this.name = name;
+		this.cost = cost;
+	}
+	
+	public Tower(String name, int cost, TowerAttack attack, TowerUpgrade upgrade1, TowerUpgrade upgrade2){
+		this.name = name;
+		this.cost = cost;
+		this.attack = attack;
+		this.upgrade1 = upgrade1;
+		this.upgrade2 = upgrade2;
+	}
+	
+	public void attackTargets(){
+		attack.attackTargets();
+	}
+	
+	public void upgrade1(int value){
+		upgrade1.upgrade(this, value);
+	}
+	
+	public void upgrade2(int value){
+		upgrade2.upgrade(this, value);
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public int getCost(){
+		return cost;
+	}
 }
--- core/src/main/java/com/rawprogramming/games/Towers/TowerStore.java
+++ core/src/main/java/com/rawprogramming/games/Towers/TowerStore.java
@@ -1,15 +1,52 @@
 package com.rawprogramming.games.Towers;
 
-public class TowerStore {
+import java.util.ArrayList;
+import com.rawprogramming.games.*;
+
+public class TowerStore extends Grid {
 	
 	private int money;
-	private Tower[] towers;
+	private ArrayList<Tower> towers;
+	private Tower selectedTower;
 	
-	public void buyTower(){
-		
+	public TowerStore(int money, int offsetX, int offsetY, GameApp game){
+		super(2, 5, offsetX, offsetY, game);
+		towers = new ArrayList<Tower>();
+		generateTowers();
+		this.money = money;
 	}
 	
-	public void placeTower(){
-		
+	public void selectTower(float x, float y){
+		GridSquare square = getTouchedSquare(x, y);
+		selectedTower = square.getTower();
+	}
+	
+	public void deselectTower(){
+		selectedTower = null;
+	}
+	
+	public Tower buyTower(){
+		money -= selectedTower.getCost();
+		return selectedTower;
+	}
+	
+	public void generateTowers(){
+		towers.add(new Tower("Basic Tower", 50));
+		for(int i = 0; i < towers.size(); i++){
+			if(i % 2 == 0){
+				getSquare(1, i/2).setTower(towers.get(i));
+			}
+			else{
+				getSquare(0, (int)Math.ceil(i/2.0)).setTower(towers.get(i));
+			}
+		}
+	}
+	
+	public boolean hasTowerSelected(){
+		return selectedTower != null;
+	}
+	
+	public void addMoney(int money){
+		this.money += money;
 	}
 }
