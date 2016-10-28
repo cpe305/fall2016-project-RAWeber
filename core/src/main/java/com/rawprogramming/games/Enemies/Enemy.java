84fe8bb81c45dc90d67377b2c92e1b84ed32be3c
Daniel Sabsay <dsabsay@calpoly.edu>
2016-10-27 18:27:45 -0700

    Implement Spawner.

--- core/src/main/java/com/rawprogramming/games/Enemies/Enemy.java
+++ core/src/main/java/com/rawprogramming/games/Enemies/Enemy.java
@@ -2,12 +2,56 @@ package com.rawprogramming.games.Enemies;
 
 public class Enemy {
 
-	private String name;
-	private int health;
-	private double speed;
-	private int reward;
-	
-	public void move(){
-		
-	}
+  private String name;
+  private int health;
+  private double speed;
+  private int reward;
+
+  public Enemy(String name, int health, double speed, int reward) {
+    this.name = name;
+    this.health = health;
+    this.speed = speed;
+    this.reward = reward;
+  }
+
+  public void spawn() {
+    System.out.println("Spawning enemy: " + this.name);
+  }
+  
+  public void move() {
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
+  }
+
+  public double getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(double speed) {
+    this.speed = speed;
+  }
+
+  public int getReward() {
+    return reward;
+  }
+
+  public void setReward(int reward) {
+    this.reward = reward;
+  }
+
 }
